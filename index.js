69d5d1d43f2f85c4523e89a73b4abf95dfa40042
Sarah Currey <curreysarah28@gmail.com>
2018-05-03 23:51:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const instruments = ["guitar", "guitar", "bass", "drums"];
 function theBeatlesPlay (musicians, instruments) {
   var array= [];
   for (let i=0; i<4; i++) {
-    array.push ('${musicians [0]} plays ${instruments)
+    array.push ('${musicians [0]} plays ${instruments [0])
   }
 }
 
