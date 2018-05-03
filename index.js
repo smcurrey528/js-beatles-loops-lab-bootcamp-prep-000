ca42b4676abc58b9542b62c60a7dfe5dafbf8017
Sarah Currey <curreysarah28@gmail.com>
2018-05-03 23:51:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function theBeatlesPlay (musicians, instruments) {
   var array= [];
   for (let i=0; i<4; i++) {
     array.push ('${musicians [0]} plays ${instruments [0]}');
-    
   }
+  return array;
 }
 
 function johnLennonFacts (array) {
