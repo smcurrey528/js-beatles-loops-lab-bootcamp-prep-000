e0517abae7565866c8ec7f52a644b084655950cd
Sarah Currey <curreysarah28@gmail.com>
2018-05-03 23:54:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,14 +10,21 @@ function theBeatlesPlay (musicians, instruments) {
   return array;
 }
 
-function johnLennonFacts (array) {
-  const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
+  }
+  return results;
 }
-function iLoveTheBeatles (number) {
-  
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
 }
